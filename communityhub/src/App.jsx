5ecd591d764331b0ce5f87d26e3c23730d91a5f7
fabby <fabianmunga163@gmail.com>
2026-04-17 19:29:09 +0000

    fix routing setup and restore working React app

--- communityhub/src/App.jsx
+++ communityhub/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
@@ -7,17 +8,17 @@ import NotFound from './pages/NotFound';
 import PostDetail from './pages/PostDetail';
 
 function App() {
-  return (
-   <Routes>
-  <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="posts" element={<Posts />} />
-    <Route path="posts/:postId" element={<PostDetail />} />
-    <Route path="about" element={<About />} />
-    <Route path="*" element={<NotFound />} />
-  </Route>
-</Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="posts" element={<Posts />} />
+                <Route path="posts/:postId" element={<PostDetail />} />
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
